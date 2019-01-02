41c97a1f8aaf1dabd4e18d8f2bab4401a2c45c53
Max Lettenberger <maxlettenberger@gmail.com>
2019-01-01 22:18:14 -0600

    Clean up and add comments to Wrapper.js

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -15,77 +15,67 @@ class Wrapper extends Component {
       status: ""
     };
 
-    // shuffle = () => {
-        
-    // }
-
-    // when the user clicks on a character card
+    // When user clicks character card
     handleClick = (e) => {
-        
+        // Prevent event default
         e.preventDefault();
-
+        // Console log the target
         console.log(e.target);
-
-        // grab the selected card's id
+        // Get the selected card's name
         let name = e.target.name;
-        //
+        // Log the name
         console.log("clicked " + name)
-        // declare array to hold changes of state
+        // Create array to hold clicked characters
         let arr = this.state.clicked;
-
+        // Handle successful click
         if (!this.state.clicked.includes(name)) {
-
-            // Push to arr
+            // Push character name to array
             arr.push(name);
-
-            // Log 
+            // Log correct guess in console
             console.log("pushed " + name)
-
-            let newScore = this.state.score;
-            newScore+=1;
-
-            // Set state
+            // Create new variable to hold new score
+            let newScore = this.state.score + 1;
+            // Update status message, clicked array and score
             this.setState({status: "Nice click"})
             this.setState({clicked: arr})
             this.setState({score: newScore})
-
+            // Handle game win
             if (newScore == 12) {
-                
+                // Update status message, clicked array and score
                 this.setState({status: "You won!!! New game started"})
                 this.setState({score: 0})
                 this.setState({clicked: []})
             }
-            
+            // Handle new top score
             if (newScore > this.state.topScore) {
+                // Update top score to match new high score
                 this.setState({topScore: newScore})
             }
-
+        // Handle wrong click
         } else {
-
-            //
-           console.log("already clicked: " + name)
-
-           //
-           this.setState({clicked: []})
-
-           this.setState({status: "You already clicked " + name + ". New game started"})
-
-           this.setState({score: 0})
+            // Console log character name
+            console.log("already clicked: " + name)
+            // Reset clicked array
+            this.setState({clicked: []})
+            // Update status message 
+            this.setState({status: "You already clicked " + name + ". New game started"})
+            // Update score
+            this.setState({score: 0})
         }
+        // Shuffle function
         /**
          * Shuffles array in place. ES6 version
          * param {Array} a items An array containing the items.
          */
+        // Create new array and fill with all character objects
         let a = this.state.characters;
-
+        // Loop through array, shuffling elements
         for (let i = a.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
                 [a[i], a[j]] = [a[j], a[i]];
         }
-
         // Assign our new random characters array to the state characters array
         this.setState({characters: a});
-
     }
   
     render() {
@@ -93,12 +83,10 @@ class Wrapper extends Component {
       return (
         <div>
          <Nav 
-
             key = {1}
             score = {this.state.score}
             topScore = {this.state.topScore}
             status = {this.state.status}
-
          />
          <Header />
           <div className="container" id="card-container">
@@ -106,13 +94,11 @@ class Wrapper extends Component {
                 {
                     this.state.characters.map(
                         character => <Card 
-
-                            key          ={character.id}
-                            id           ={character.id}
-                            name         ={character.name}
-                            image        ={character.image}
-                            handleClick  ={this.handleClick}
-
+                            key = {character.id}
+                            id = {character.id}
+                            name = {character.name}
+                            image = {character.image}
+                            handleClick = {this.handleClick}
                         />
                     ) 
                 }
@@ -121,7 +107,7 @@ class Wrapper extends Component {
         </div>
       );
     }
-  }
+}
   
-  export default Wrapper;
+export default Wrapper;
   
